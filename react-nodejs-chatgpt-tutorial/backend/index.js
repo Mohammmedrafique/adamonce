988a85bab2ca21c665c7a09f77eafdb84104348d
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-11-12 16:45:37 +0530

    d

---
+++ react-nodejs-chatgpt-tutorial/backend/index.js
@@ -0,0 +1,39 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  organization: "org-IYPoSttaEuqUUAm95CFhHkXn",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          " I want you to act as an expert frontend software developer. You are asked to take my technical interview for the position of a frontend software developer and share your feedback. Ask me one question about React at a time, and wait for my response before moving on to the next question ",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
