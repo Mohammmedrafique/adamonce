fd8e8ff61bb028aca930410e2d771d8ba07dd850
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-11-15 04:11:58 +0530

    d

--- React-ai-interview-bot/backend/index.js
+++ React-ai-interview-bot/backend/index.js
@@ -85,13 +85,6 @@ import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 const app = express();
 
 // Middleware
