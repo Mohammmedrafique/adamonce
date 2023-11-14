0d61a622c8219c12607073f6a82a0265e3a7de31
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-11-15 04:42:01 +0530

    d

--- React-ai-interview-bot/backend/index.js
+++ React-ai-interview-bot/backend/index.js
@@ -9,6 +9,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5174");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(cors());
 app.use(bodyParser.json());
 
